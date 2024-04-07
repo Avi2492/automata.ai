b4f86bf38f95e7fc158f4e4270c958643a90cb5c
Avinash Pandey <avinash.12009084@lpu.in>
2024-04-07 13:10:01 +0530

    Sidebar Complete

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -6,22 +6,29 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { menuOptions } from "@/lib/constants";
-import { RiBardFill } from "@remixicon/react";
+import { Separator } from "@radix-ui/react-separator";
+import {
+  RiBardFill,
+  RiCursorLine,
+  RiDatabase2Fill,
+  RiGitBranchFill,
+  RiSlackFill,
+} from "@remixicon/react";
 import clsx from "clsx";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ModeToggle } from "../global/mode-toggle";
 
 interface Props {}
 
 const MenuOptions = (props: Props) => {
   const pathName = usePathname();
   return (
-    <nav className="dark:bg-black h-screen overflow-scroll justify-between flex items-center flex-col gap-10 py-6 px-4">
+    <nav className="dark:bg-black h-screen overflow-scroll justify-between flex items-center flex-col gap-10 py-6 px-4 md:p-4">
       <div className="flex items-center justify-center flex-col gap-8">
         <Link href={"/"} className="flex font-bold flex-row gap-1">
           <RiBardFill />
-          <p className="text-xl font-bold">Automata.ai</p>
         </Link>
         <TooltipProvider>
           {menuOptions.map((menuItem) => (
@@ -45,10 +52,37 @@ const MenuOptions = (props: Props) => {
                     </Link>
                   </li>
                 </TooltipTrigger>
+                <TooltipContent
+                  side="right"
+                  className="bg-black/10 backdrop-blur-xl"
+                >
+                  <p>{menuItem.name}</p>
+                </TooltipContent>
               </Tooltip>
             </ul>
           ))}
         </TooltipProvider>
+        <Separator />
+        <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-56 overflow-scroll border-[1px]">
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <RiCursorLine className="dark:text-white" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]" />
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <RiGitBranchFill className="dark:text-white" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]" />
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <RiSlackFill className="dark:text-white" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]" />
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <RiDatabase2Fill className="dark:text-white" size={18} />
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center flex-col gap-8">
+        <ModeToggle />
       </div>
     </nav>
   );
